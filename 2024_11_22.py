f907571b2c5454232c2dda1509dd64b067049c25
gyb357 <gyb357@naver.com>
2024-11-22 21:26:48 +0900

    no message

--- 2024_11_22.py
+++ 2024_11_22.py
@@ -1,1 +1,28 @@
 # %%
+# %pip install streamlit
+
+# %%
+import streamlit as st
+
+# %%
+st.title('My first app')
+
+# %%
+st.text('Hello, world!')
+
+# %%
+st.markdown('Streamlit is **_really_ cool**.')
+
+# %%
+st.latex('a+ar + a r^2 + a r^3')
+
+# %%
+prompt = st.chat_input('Say something')
+# editor_text = st.text_area('Editor', 'Type here...', key='editor1', value='default text')
+editor_text = st.text_area('Editor', 'Type here...', key='editor1')
+
+if prompt:
+    print(prompt)
+    st.text(f'You said: {prompt}')
+
+# %%
